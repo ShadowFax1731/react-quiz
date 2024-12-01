40ee26d76080e6bc85c15dda376a2a1fdbe34aa6
shadowfax <55938330+ShadowFax1731@users.noreply.github.com>
2024-12-01 05:31:59 +0530

    fetching questions from a mock API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import MainContent from "./MainContent";
+
+const initialState = {
+  questions: [],
+
+  //loading, error, ready, active, finished -> status values
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown Action");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <>
       <div className="app">
