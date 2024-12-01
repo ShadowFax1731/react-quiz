df35c7a09b6c2de7b0e0db2af480495540aa8747
shadowfax <55938330+ShadowFax1731@users.noreply.github.com>
2024-12-02 02:13:41 +0530

    Displaying Progress

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
 
 const initialState = {
   questions: [],
@@ -45,12 +46,13 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPoints = questions.reduce((prev, curr) => prev + curr.points, 0);
 
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -72,6 +74,13 @@ function App() {
           )}
           {status === "active" && (
             <>
+              <Progress
+                index={index}
+                numQuestions={numQuestions}
+                points={points}
+                maxPoints={maxPoints}
+                answer={answer}
+              />
               <Question
                 question={questions[index]}
                 dispatch={dispatch}
